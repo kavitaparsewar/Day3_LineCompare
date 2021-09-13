0c781c651d86230bcebb79e8c8fb1e44545ecb3c
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-13 21:06:22 +0530

    Welcome to Line comparison computation program

---
+++ Welcome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3_LineCompare
+{
+    class Welcome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
